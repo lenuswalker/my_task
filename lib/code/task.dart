1e24b63f41d7148a8756ddb8d30c1a716fbbed01
Lenus Walker <walker.lenus@gmail.com>
2018-05-10 16:55:10 -0400

    Create Task Screen

--- lib/code/task.dart
+++ lib/code/task.dart
@@ -1,3 +1,9 @@
 class Task {
+  final String subject;
+  final String description;
 
+  Task({
+    this.subject,
+    this.description
+  });
 }
--- lib/screens/create_task.dart
+++ lib/screens/create_task.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
 
-class CreateTask extends StatelessWidget {
+class CreateTask extends StatefulWidget {
+  @override
+  _CreateTaskState createState() => new _CreateTaskState();
+}
+
+class _CreateTaskState extends State<CreateTask> {
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    final Size screenSize = MediaQuery.of(context).size;
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Create Task'),
       ),
       body: new Container(
-
+        padding: new EdgeInsets.all(20.0),
+        child: Form(
+          key: this._formKey,
+          child: new ListView(
+            children: <Widget>[
+              new TextFormField(
+                keyboardType: TextInputType.text,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter a Task Name';
+                  }
+                },
+                decoration: new InputDecoration(
+                  hintText: 'Task Name'
+                ),
+              ),
+              const SizedBox(height: 24.0),
+              new TextFormField(
+                decoration: const InputDecoration(
+                  border: const OutlineInputBorder(),
+                ),
+                maxLines: 5,
+              ),
+              new Container(
+                width: screenSize.width,
+                child: new RaisedButton(
+                  child: new Text('Submit'),
+                  onPressed: null
+                ),
+                margin: new EdgeInsets.only(
+                  top: 20.0
+                ),
+              )
+            ],
+          )
+        ),
       ),
     );
   }
